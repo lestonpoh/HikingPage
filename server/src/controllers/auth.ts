f97c4cba0bbdba1e11dff3e17f2b00f22cd500b8
Wang Anqi <129874349+lestonpoh@users.noreply.github.com>
2025-01-18 19:24:58 +0800

    added express validator

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,25 +1,39 @@
 import { Request, Response } from "express";
 import { db } from "../connect";
 import bcrypt from "bcryptjs";
+import { body, validationResult } from "express-validator";
 
-export const register = (req: Request, res: Response) => {
-  // check if username exists
+const validateRegister = [
+  body("username").notEmpty(),
+  body("email").notEmpty().isEmail(),
+  body("password").notEmpty(),
+];
 
-  const q = "SELECT * FROM user WHERE username = ?";
-  db.query(q, [req.body.username], (err, data) => {
-    if (err) return res.status(500).json(err);
-    if (data.length) return res.status(409).json("User already exist!");
-    // hashPassword
-    const salt = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
-    const q = "INSERT INTO users (`username`,`email`,`password`) VALUE (?)";
-    const reqValues = [req.body.username, req.body.email, hashedPassword];
-    db.query(q, reqValues, (err, data) => {
+export const register = [
+  validateRegister,
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // check if username exists
+    const q = "SELECT * FROM user WHERE username = ?";
+    db.query(q, [req.body.username], (err, data) => {
       if (err) return res.status(500).json(err);
-      return res.status(200).json("User has been created.");
+      if (data.length) return res.status(409).json("User already exist!");
+      // hashPassword
+      const salt = bcrypt.genSaltSync(10);
+      const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+      const q = "INSERT INTO users (`username`,`email`,`password`) VALUE (?)";
+      const reqValues = [req.body.username, req.body.email, hashedPassword];
+      db.query(q, reqValues, (err, data) => {
+        if (err) return res.status(500).json(err);
+        return res.status(200).json("User has been created.");
+      });
     });
-  });
-};
+  },
+];
 
 export const login = (req: Request, res: Response) => {};
 
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -3,8 +3,8 @@ import { login, logout, register } from "../controllers/auth";
 
 const router = Router();
 
-router.get("/register", register);
-router.get("/login", login);
+router.post("/register", register);
+router.post("/login", login);
 router.post("/logout", logout);
 
 export default router;
